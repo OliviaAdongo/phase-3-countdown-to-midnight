1616e2f2b76dddfa695c944c061362fe3adc34e6
Olivia Adongo <akinyioliviatove@gmail.com>
2022-08-22 10:52:11 +0300

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,27 @@
 #write your code here
 
-def countdown
+# def countdown (number= 10)
+# puts "#{number} SECOND(S)!"
+
+# end
+
+def countdown (n)
+     while n >= 1
+     puts "#{n} SECOND(S)!"
+     n -=1 
+     end
+     "HAPPY NEW YEAR !"
 end
+# q = countdown(10)
+# puts q
+
+def countdown_with_sleep (n)
+    while n >= 1
+        puts "#{n} SECOND(S)!"
+       sleep (1)
+        n -=1 
+        end
+        "HAPPY NEW YEAR !"
+    
+end
+countdown_with_sleep(10)
