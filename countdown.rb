ba2366a54cea2d8088355b66907c9a837cda2c07
Olivia Adongo <akinyioliviatove@gmail.com>
2022-08-22 10:47:55 +0300

    commit tree 2d3e23e021fb94aa7b7e9c4f648284b987510221

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
 #write your code here
 
-def countdown
+# def countdown (number= 10)
+# puts "#{number} SECOND(S)!"
+
+# end
+
+def countdown (n)
+     while n >= 1
+     puts "#{n} SECOND(S)!"
+     n -=1 
+     end
+    return "HAPPY NEW YEAR !"
 end
+q = countdown(11)
